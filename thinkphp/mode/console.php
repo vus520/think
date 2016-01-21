ee57825a293db62ff26d7183c7d745523d93cb39
云无心 <448901948@qq.com>
2016-01-21 13:06:53 +0800

    更新入口文件

--- thinkphp/mode/console.php
+++ thinkphp/mode/console.php
@@ -13,33 +13,10 @@
  * ThinkPHP CLI模式定义
  */
 
-use think\Console;
-
-\think\Response::tramsform(function () {
-});
-
 return [
-    // 配置文件
-    'config' => array_merge(include THINK_PATH . 'convention' . EXT, [
-        'default_return_type' => ''
-    ]),
 
-    'commands' => [],
-    'run'      => function () {
-        $console = new Console('Think Console', '0.1');
-
-
-        $commands = \think\Config::get('commands');
-
-        if (is_array($commands)) {
-            foreach ($commands as $command) {
-                if (class_exists($command) && $command instanceof \think\console\command\Command) {
-                    $console->add(new $command());
-                }
-            }
-        }
-
-        $console->run();
-    }
+    'config' => [
+        'commands' => []
+    ]
 
 ];
